f06770e01bc2735c0609757eede9b49f0a0706b9
SethuramanAnirudh <anirudhsethuraman.00@gmail.com>
2026-01-05 23:16:08 +0530

    day4

--- day4/random_number/src/main.rs
+++ day4/random_number/src/main.rs
@@ -1,21 +1,31 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-
 fn main() {
-    println!("Guess the numnber between 1 and 100!");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please enter a valid number");
-    let secret_no = rand::thread_rng().gen_range(1..=100);
-    println!("the secret number is : {secret_no}");
+    loop {
+        println!("Guess the numnber between 1 and 100!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please enter a valid number");
+                continue;
+            }
+        };
+        let secret_no = rand::thread_rng().gen_range(1..=100);
+        println!("the secret number is : {secret_no}");
 
-    println!("You guessed : {guess}");
-    match guess.cmp(&secret_no) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed : {guess}");
+        match guess.cmp(&secret_no) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
