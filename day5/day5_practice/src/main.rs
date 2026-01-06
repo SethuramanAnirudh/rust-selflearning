e9702308dcc2a8ad53871a9ddeaf4e4728eb04e3
SethuramanAnirudh <anirudhsethuraman.00@gmail.com>
2026-01-06 23:08:30 +0530

    day5

---
+++ day5/day5_practice/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut x = 5; // mut makes the variable mutable
+                   // by default variables are immutable
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+}
