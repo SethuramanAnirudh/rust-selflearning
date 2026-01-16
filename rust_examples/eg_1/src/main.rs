18caf01cfcfaaedf93165e91eaab2db2592044f0
SethuramanAnirudh <anirudhsethuraman.00@gmail.com>
2026-01-16 23:24:39 +0530

    practice problems

---
+++ rust_examples/eg_1/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // println!("Hello, world!");
+    test_sum_array();
+}
+// #[test]
+fn test_sum_array() {
+    assert_eq!(sum_array(&[1,2,3,4,5,6]),21);
+    assert_eq!(sum_array(&[]),0);
+    assert_eq!(sum_array(&[-1,1,-1,1]),0);
+}
+fn sum_array(arr: &[i32]) -> i32 {
+    let mut sum = 0;
+    let mut index = 0;
+    while index < arr.len() {
+        sum += arr[index];
+        index += 1;
+    }
+    sum
+}
