e3e7846dfcc34fb283653ff970c6a86162dcc677
SethuramanAnirudh <anirudhsethuraman.00@gmail.com>
2026-01-17 21:53:19 +0530

    added comments

--- rust_examples/eg_2/src/main.rs
+++ rust_examples/eg_2/src/main.rs
@@ -3,6 +3,8 @@ fn test_max_element() {
     assert_eq!(max_element(&[1,2,3,4,5,33]),Some(33));
     assert_eq!(max_element(&[]),None);
 }
+// This function may or may not be able to return an i32.
+// If the input array is empty, it returns None. Otherwise, it returns Some(maximum value).”
 fn max_element(arr: &[i32]) -> Option<i32> {
     if arr.is_empty() {
         return None;
@@ -16,4 +18,9 @@ fn max_element(arr: &[i32]) -> Option<i32> {
         index += 1;
     }
     Some(max)
+}
+/*
+enum Option<T> {
+    Some(T),
+    None,
 }
