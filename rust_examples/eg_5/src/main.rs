d4008520484e442b032c6eb90f59dbf88d3f7440
SethuramanAnirudh <anirudhsethuraman.00@gmail.com>
2026-01-22 23:45:54 +0530

    count words in sentence

--- rust_examples/eg_5/src/main.rs
+++ rust_examples/eg_5/src/main.rs
@@ -7,6 +7,18 @@ fn main() {
     println!("Is only digits: {}", only_digits);
     let not_only_digits = is_only_digits(&String::from("Ani12345"));
     println!("Is only digits: {}", not_only_digits);
+
+    let word_count = count_words(&String::from("Hello world this is Rust"));
+    println!("Number of words: {}", word_count);
+
+    let word_count2 = count_words(&String::from("   Leading and trailing spaces   "));
+    println!("Number of words: {}", word_count2);
+
+    let word_count3 = count_words(&String::from(""));
+    println!("Number of words: {}", word_count3);
+
+    let word_count4 = count_words(&String::from("SingleWord"));
+    println!("Number of words: {}", word_count4);
 }
 fn count_vowels(s: &String) -> usize {
     let mut count = 0;
@@ -45,4 +57,17 @@ fn is_only_digits(s: &String) -> bool {
         }
     }
     return true;
+}
+fn count_words(s: &String) -> usize {
+    let mut count = 0;
+    let mut in_word = false;
+    for c in s.chars() {
+        if c != ' ' && !in_word {
+            count += 1;
+            in_word = true;
+        } else if c == ' ' {
+            in_word = false;
+        }
+    }
+    count
 }
